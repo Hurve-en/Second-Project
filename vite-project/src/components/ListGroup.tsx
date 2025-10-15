0c7128cff1fc1e1aa9a7c41faa0bbac3b747eff1
Hurve-en <142562684+Hurve-en@users.noreply.github.com>
2025-10-15 22:01:01 +0800

    initial commit

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -1,14 +1,36 @@
-function ListGroup() {
-  let items = ["New york", "San Francisco", "Tokyo", "London", "Paris"];
-  items = [];
+interface Props {
+  items: string[];
+  heading: string; 
+  onSelectItem: (item: string) => void;// (item: string) => void
+}
 
+import { useState } from "react";
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+
+  
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found </p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
